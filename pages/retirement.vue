2522997c8d7bb17def07bfd95416462a98a7db71
russo_zhang <russo_zhang@163.com>
2025-07-09 17:58:56 +0800

    收支差额

--- pages/retirement.vue
+++ pages/retirement.vue
@@ -26,25 +26,39 @@
             <el-form-item label="月支出">
                 <el-input-number v-model="form.monthlyExpense" :min="0" :step="1000" placeholder="请输入月支出" />
             </el-form-item>
-            <el-form-item label="每月存款">
-                <div>
-                    <p>税后工资 - 支出 ： {{ formatMoney(monthlySaving) }}</p>
-                    <p>利息所得 ： {{ formatMoney(monthlyInterest) }}</p>
-                    <p>
-                        月总存款：
-                        {{ formatMoney(monthlyInterest + monthlySaving) }}
-                    </p>
-                </div>
-            </el-form-item>
         </el-form>
         <div v-if="result.totalNeeded > 0" class="result-block">
             <el-divider>计算结果</el-divider>
+            <div>
+                收支差额
+                <el-tooltip
+                    class="box-item"
+                    effect="dark"
+                    content="收支差额 = 月税后工资 - 月支出"
+                    placement="top-start"
+                >
+                    <svg style="width: 16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
+                        <path
+                            fill="currentColor"
+                            d="M512 64a448 448 0 1 1 0 896.064A448 448 0 0 1 512 64m67.2 275.072c33.28 0 60.288-23.104 60.288-57.344s-27.072-57.344-60.288-57.344c-33.28 0-60.16 23.104-60.16 57.344s26.88 57.344 60.16 57.344M590.912 699.2c0-6.848 2.368-24.64 1.024-34.752l-52.608 60.544c-10.88 11.456-24.512 19.392-30.912 17.28a12.992 12.992 0 0 1-8.256-14.72l87.68-276.992c7.168-35.136-12.544-67.2-54.336-71.296-44.096 0-108.992 44.736-148.48 101.504 0 6.784-1.28 23.68.064 33.792l52.544-60.608c10.88-11.328 23.552-19.328 29.952-17.152a12.8 12.8 0 0 1 7.808 16.128L388.48 728.576c-10.048 32.256 8.96 63.872 55.04 71.04 67.84 0 107.904-43.648 147.456-100.416z"
+                        ></path>
+                    </svg>
+                </el-tooltip>
+                ：
+                <b>{{ formatMoney(monthlySaving) }} 元</b>
+            </div>
+            <div>
+                利息所得 ：<b>{{ formatMoney(monthlyInterest) }} 元</b>
+            </div>
+            <div style="color: skyblue">
+                月总存款：<b>{{ formatMoney(monthlyInterest + monthlySaving) }} 元</b>
+            </div>
             <div>
                 覆盖月支出所需总存款：<b>{{ formatMoney(result.totalNeeded) }}</b> 元 （约
                 <b>{{ formatMoney(result.totalNeeded / 10000) }}</b> 万）
             </div>
             <div>
-                当前年化收益：<b>{{ formatMoney(annualReturnMoney) }} 元</b>
+                本年度年化收益：<b>{{ formatMoney(annualReturnMoney) }} 元</b>
             </div>
             <div>
                 剩余工作时间：<b>{{ formatMoney(result.monthsLeft) }}</b> 个月（约 <b>{{ result.yearsLeft }}</b> 年）
@@ -150,6 +164,11 @@ const formatMoney = (value: number) => {
         background: #f8f8f8;
         padding: 20px;
         border-radius: 8px;
+        > div {
+            margin-bottom: 10px;
+            svg {
+            }
+        }
     }
 }
 </style>
